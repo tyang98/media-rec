c6842e7017223bb12e88ccebf12e192e66124681
Tony <tony.yang98@gmail.com>
2020-08-13 16:56:13 -0400

    finalize homepage

--- media-rec/src/components/Home.js
+++ media-rec/src/components/Home.js
@@ -15,51 +15,49 @@ function Home({ handleLogin }) {
       <img src={background} style={{ minHeight: '100%', minWidth: '100%', position: 'fixed', top: '0', left: '0', zIndex: '-1', filter: 'brightness(50%)' }} />
       <div className="container" styles={{ display: 'flex', justifyContent: 'center' }}>
 
-        <img src={transparentLogo} className="center mx-auto" style={{ maxWidth: '750px', display: 'block', position: 'relative', top: '-60px' }} />
+        <img src={transparentLogo} className="center mx-auto" style={{ maxWidth: '80%', display: 'block', position: 'relative', bottom: '6vw' }} />
         <Row >
-          <Col className='col-md-4' style={{ position: 'relative', left: '-150px' }}>
-            <Card style={{ width: '450px', height: '350px', position: 'relative', top: '-100px' }}>
+          <Col className='col-md-4' style={{ position: 'relative', right: '2vw' }}>
+            <Card style={{ width: '100%', height: '100%', position: 'relative', bottom: '12vw', marginBottom: '2%' }}>
               <Card.Img variant='top' src={card1} />
-              <Card.Title>Integrated with Spotify</Card.Title>
-              <Card.Text>Search Your Favorite Songs for Recommendations</Card.Text>
+              <Card.Title style={{ marginLeft: '2%' }}>Integrated with Spotify</Card.Title>
+              <Card.Subtitle style={{ marginLeft: '2%' }}>Search Songs for Recommendations</Card.Subtitle>
             </Card>
           </Col>
 
           <Col className='col-md-4'>
-            <Card style={{ width: '450px', height: '350px', position: 'relative', top: '-100px' }}>
+            <Card style={{ width: '100%', height: '100%', position: 'relative', bottom: '12vw', marginBottom: '2%' }}>
               <Card.Img variant='top' src={card2} />
-              <Card.Title>Expand your library</Card.Title>
-              <Card.Text>Add Recommended Songs to Your Playlists</Card.Text>
+              <Card.Title style={{ marginLeft: '2%' }}>Expand your library</Card.Title>
+              <Card.Subtitle style={{ marginLeft: '2%' }}>Add Recommended Songs to Your Playlists</Card.Subtitle>
             </Card>
           </Col>
 
-          <Col className='col-md-4' style={{ position: 'relative', right: '-150px' }}>
-            <Card style={{ width: '450px', height: '350px', position: 'relative', top: '-100px' }}>
+          <Col className='col-md-4' style={{ position: 'relative', left: '2vw' }}>
+            <Card style={{ width: '100%', height: '100%', position: 'relative', bottom: '12vw', marginBottom: '2%' }}>
               <Card.Img variant='top' src={card3} />
-              <Card.Title>Watch new titles</Card.Title>
-              <Card.Text>Search Your Favorite Movie for Recommendations</Card.Text>
+              <Card.Title style={{ marginLeft: '2%' }}>Watch new titles</Card.Title>
+              <Card.Subtitle style={{ marginLeft: '2%' }}>Search Movies for Recommendations</Card.Subtitle>
             </Card>
           </Col>
-
+          <h4 className="mx-auto" style={{ textAlign: 'center', color: '#fff', position: 'relative', bottom: '8vw' }}>
+            <b> To get started, choose either songs or movies to get recommendations! </b>
+          </h4>
         </Row>
-
-        <h4 style={{ textAlign: 'center', color: '#fff' }}>
-          <b> To get started, choose either songs or movies to get recommendations! </b>
-        </h4>
       </div >
-      <Container className="mx-auto mt-4">
+      <Container className="mx-auto">
         <Row>
-          <Col>
+          <Col style={{ position: 'relative', bottom: '6vw' }}>
             <Button size="lg" className="col-md-12" onClick={handleLogin}> Songs </Button>
           </Col>
-          <Col>
+          <Col style={{ position: 'relative', bottom: '6vw' }}>
             <NavLink to="/moviesrec">
               <Button size="lg" className="col-md-12" >Movies</Button>
             </NavLink>
           </Col>
         </Row>
       </Container>
-    </div>
+    </div >
   );
 }
 
